241127ea11782e3187c2ed9af6d9ca73215f1bff
xtrmntr80 <47057709+xtrmntr80@users.noreply.github.com>
2025-09-22 00:33:33 +0900

    Create sw.js
    sw.js（Service Worker）をリポジトリ直下に追加

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = "drawer-kun-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./script/script.js",
+  "./styles/styles.css",
+  "./icon/icon-192.png",
+  "./icon/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
